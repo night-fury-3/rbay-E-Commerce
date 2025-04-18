272c1d10c96c7d67236f8339cd9ca24e339b07d2
night-fury-3 <jonathonfrakes3@gmail.com>
2025-04-18 05:47:23 -0400

    feat: storing usernames, kind of storing strings in sorted sets

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey, usernameUniqueKey } from '$services/keys';
+import { usersKey, usernameUniqueKey, usernamesKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -24,6 +24,11 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 	await client.hSet(usersKey(id), serialize(attrs));
 	await client.sAdd(usernameUniqueKey(), attrs.username);
+
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16) //info: string to number conversion
+	});
 
 	return id;
 };
