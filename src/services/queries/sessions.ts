b121d62d7ed4467689c8ece73bd17cf16c2edc0d
night-fury-3 <jonathonfrakes3@gmail.com>
2025-04-17 22:25:21 -0400

    feat: Add Serialize

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,13 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKey(id));
+
+	console.log(session);
+
+	return session;
+};
 
 export const saveSession = async (session: Session) => {};
